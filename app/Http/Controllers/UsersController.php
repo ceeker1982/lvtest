4d96436cf7c6ebdf272357ad187243b68a321188
Ceeker_office <ceeker@126.com>
2021-06-24 07:15:44 +0000

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -13,7 +13,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth', [            
-            'except' => ['show', 'create', 'store']
+            'except' => ['show', 'create', 'store', 'index']
         ]);
 		
 		 $this->middleware('guest', [
@@ -48,6 +48,12 @@ class UsersController extends Controller
         Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
+    }
+	
+	public function index()
+    {
+        $users = User::paginate(6);
+        return view('users.index', compact('users'));
     }
 	
 	public function edit(User $user)
---
+++ resources/views/users/_user.blade.php
@@ -0,0 +1,5 @@
+<div class="list-group-item">  
+  <a href="{{ route('users.show', $user) }}">
+    {{ $user->name }}
+  </a>
+</div>
