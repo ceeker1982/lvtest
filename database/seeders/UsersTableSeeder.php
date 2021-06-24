c296d70056d5a88ae415db62aa414495baeda230
Ceeker_office <ceeker@126.com>
2021-06-24 07:39:03 +0000

    管理员可删除用户

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -20,6 +20,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'kevin';
         $user->email = 'ceeker@126.com';
+		$user->is_admin = true;
         $user->save();
     }
 }
